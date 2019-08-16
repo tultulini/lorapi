a21d7b6dbb18e7d8be7262070a79e487e9bf2c2b
tultulini <fisher.tal@gmail.com>
2019-08-16 10:44:10 -0300

    added CORS

--- src/app.js
+++ src/app.js
@@ -31,12 +31,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Methods", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 setupRouting();
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
+
+
 
 // error handler
 app.use(function (err, req, res, next) {
